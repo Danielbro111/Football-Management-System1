c4a160de6277f1cffdeb15360ffa65c2a5f441ec
daniel <20107405@mail.wit.ie>
2024-11-08 16:59:04 +0000

    Add data class model for Team Add data class model for Players

--- src/main/kotlin/models/Players.kt
+++ src/main/kotlin/models/Players.kt
@@ -1,5 +1,12 @@
 package ie.setu.models
 
-data class Players{
+data class Players(
 
-}
+      val Name :String,
+      val Number: Int,
+      val height: Double,
+      val weight: Double,
+      val Position: String,
+      val Nationality: String,
+
+      )
--- src/main/kotlin/models/Team.kt
+++ src/main/kotlin/models/Team.kt
@@ -1,4 +1,10 @@
 package ie.setu.models
 
-class Team {
-}
+data class Team (
+    val Name: String,
+    val Manager: String,
+    val Captain: String,
+    val League: String,
+    val Trophies: Int,
+
+)
